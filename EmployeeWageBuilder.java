4eb1da3099cea668e8759af03aa04572149b7f79
mihirpatil0 <mihirrpatil@gmail.com>
2021-05-31 18:25:06 +0530

    Checking: If employee is fulltime present or absent

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,7 +1,16 @@
 package empwage;
 
-public class EmployeeWageBuilder {
-	public static void main(String[] args) {
+public class EmployeeWageBuilder 
+{
+	public static void main(String[] args) 
+	{
 		System.out.println("Welcome to Employee Wage Computation Program");
+		int isPresent = 1;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		//System.out.println("Random number generated is" + " " + empCheck );
+		if (empCheck == isPresent)
+			System.out.println("Employee is Full Time Present!");
+		else
+			System.out.println("Employee is Absent!");
 	}
 }
