c0c29cf8d990c5a73c9bdd4d77900cd78a9e5989
mihirpatil0 <mihirrpatil@gmail.com>
2021-05-31 18:51:01 +0530

    Added: Daily Wage Calculation for Employees

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -6,11 +6,21 @@ public class EmployeeWageBuilder
 	{
 		System.out.println("Welcome to Employee Wage Computation Program");
 		int isPresent = 1;
+		int wagePerHr = 20;
+		int fullDayHr = 8;
+		int finalWage;
 		double empCheck = Math.floor(Math.random()*10)%2;
 		//System.out.println("Random number generated is" + " " + empCheck );
 		if (empCheck == isPresent)
+		{
+			finalWage = (wagePerHr * fullDayHr );
 			System.out.println("Employee is Full Time Present!");
+			System.out.println("Daily wage of an Full timer employee is" + " " + finalWage);
+		}
 		else
+		{
 			System.out.println("Employee is Absent!");
+	
+		}
 	}
 }
